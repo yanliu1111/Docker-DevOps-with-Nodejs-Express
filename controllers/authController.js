9b6cf76c8f61dbb7d5f8ac37afbe194d1092a0ba
yanliu1111 <liuyan0504@gmail.com>
2023-04-29 18:42:04 -0600

    feat:hashpassword done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,13 @@
 import User from "../models/userModel.js";
-
+import bcrypt from "bcryptjs";
 const signUp = async (req, res, next) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 12);
   try {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+      username,
+      password: hashedPassword,
+    });
     res.status(201).json({
       status: "success",
       data: {
